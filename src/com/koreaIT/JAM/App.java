538d157badabae3fec73e48420ada82bd1f84b01
devCJH <http-@naver.com>
2024-05-23 13:48:09 +0900

    article list에 적용

--- src/com/koreaIT/JAM/App.java
+++ src/com/koreaIT/JAM/App.java
@@ -2,11 +2,10 @@ package com.koreaIT.JAM;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import com.koreaIT.JAM.dto.Article;
@@ -59,50 +58,31 @@ public class App {
 					System.out.printf("%d번 게시물이 작성되었습니다\n", id);
 					
 				} else if (cmd.equals("article list")) {
-					System.out.println("== 게시물 목록 ==");
 					
-			    	ResultSet rs = null;
+//			    	ResultSet rs = null;
 			        
 			    	List<Article> articles = new ArrayList<>();
 			    	
-			        try {
-			            String sql = "SELECT * FROM article";
-			            sql += " ORDER BY id DESC;";
-			            
-			            pstmt = connection.prepareStatement(sql);
-			            rs = pstmt.executeQuery();
-			            
-			            while (rs.next()) {
-			            	int id = rs.getInt("id");
-			            	String regDate = rs.getString("regDate");
-			            	String updateDate = rs.getString("updateDate");
-			            	String title = rs.getString("title");
-			            	String body = rs.getString("body");
-			            	
-			            	Article article = new Article(id, regDate, updateDate, title, body);
-			            	articles.add(article);
-			            }
-			            
-			        } catch (SQLException e) {
-			            e.printStackTrace();
-			        } finally {
-			        	if (rs != null) {
-			        		try {
-			        			rs.close();
-			        		} catch (SQLException e) {
-			        			e.printStackTrace();
-			        		}
-			        	}
-			        }
+			    	SecSql sql = new SecSql();
+			    	sql.append("SELECT * FROM article");
+			    	sql.append("ORDER BY id DESC");
+			    	
+			    	List<Map<String, Object>> articleListMap = DBUtil.selectRows(connection, sql);
+			    	
+			    	for (Map<String, Object> articleMap : articleListMap) {
+			    		articles.add(new Article(articleMap));
+			    	}
 					
 					if (articles.size() == 0) {
 						System.out.println("게시물이 존재하지 않습니다");
 						continue;
 					}
 					
-					System.out.println("번호	|	제목");
+			    	System.out.println("== 게시물 목록 ==");
+					System.out.println("번호	|		제목		|		작성일		");
+					
 					for (Article article : articles) {
-						System.out.printf("%d	|	%s\n", article.id, article.title);
+						System.out.printf("%d	|	%s	|	%s	\n", article.id, article.title, article.regDate);
 					}
 					
 				} else if (cmd.startsWith("article modify ")) {
@@ -138,13 +118,6 @@ public class App {
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
 			if (connection != null) {
                 try {
                     connection.close();
