e6698568f7ee70915410ec9d08a2cd0d2d24c931
devCJH <http-@naver.com>
2024-05-23 16:50:05 +0900

    member join

--- src/com/koreaIT/JAM/App.java
+++ src/com/koreaIT/JAM/App.java
@@ -38,13 +38,88 @@ public class App {
 					break;
 				}
 				
-				if (cmd.equals("article write")) {
+				if (cmd.equals("member join")) {
+					String loginId = null;
+					String loginPw = null;
+					String loginPwChk = null;
+					String name = null;
+					
+					System.out.println("== 회원 가입 ==");
+					
+					while (true) {
+						System.out.printf("아이디 : ");
+						loginId = sc.nextLine().trim();
+						
+						if (loginId.length() == 0) {
+							System.out.println("아이디는 필수 입력 정보입니다");
+							continue;
+						}
+						
+						SecSql sql = new SecSql();
+						sql.append("SELECT COUNT(id) > 0");
+						sql.append("FROM `member`");
+						sql.append("WHERE loginId = ?", loginId);
+						
+						boolean isLoginIdDup = DBUtil.selectRowBooleanValue(connection, sql);
+						
+						if (isLoginIdDup) {
+							System.out.printf("[ %s ] 은(는) 이미 사용중인 아이디입니다\n", loginId);
+							continue;
+						}
+						
+						System.out.printf("[ %s ] 은(는) 사용가능한 아이디입니다\n", loginId);
+						break;
+					}
+					
+					while (true) {
+						System.out.printf("비밀번호 : ");
+						loginPw = sc.nextLine().trim();
+						
+						if (loginPw.length() == 0) {
+							System.out.println("비밀번호는 필수 입력 정보입니다");
+							continue;
+						}
+						
+						System.out.printf("비밀번호 확인 : ");
+						loginPwChk = sc.nextLine().trim();
+						
+						if (loginPw.equals(loginPwChk) == false) {
+							System.out.println("비밀번호가 일치하지 않습니다");
+							continue;
+						}
+						break;
+					}
+					
+					while (true) {
+						System.out.printf("이름 : ");
+						name = sc.nextLine().trim();
+						
+						if (name.length() == 0) {
+							System.out.println("이름은 필수 입력 정보입니다");
+							continue;
+						}
+						break;
+					}
+					
+		        	SecSql sql = new SecSql();
+		        	sql.append("INSERT INTO `member`");
+		        	sql.append("SET regDate = NOW()");
+		        	sql.append(", updateDate = NOW()");
+		        	sql.append(", loginId = ?", loginId);
+		        	sql.append(", loginPw = ?", loginPw);
+		        	sql.append(", `name` = ?", name);
+		        	
+		        	DBUtil.insert(connection, sql);
+
+					System.out.printf("[ %s ] 님의 가입을 환영합니다~\n", loginId);
+					
+				} else if (cmd.equals("article write")) {
 					System.out.println("== 게시물 작성 ==");
 					
 					System.out.printf("제목 : ");
-					String title = sc.nextLine();
+					String title = sc.nextLine().trim();
 					System.out.printf("내용 : ");
-					String body = sc.nextLine();
+					String body = sc.nextLine().trim();
 					
 		        	SecSql sql = new SecSql();
 		        	sql.append("INSERT INTO article");
@@ -126,9 +201,9 @@ public class App {
 					System.out.println("== 게시물 수정 ==");
 					
 					System.out.printf("수정할 제목 : ");
-					String title = sc.nextLine();
+					String title = sc.nextLine().trim();
 					System.out.printf("수정할 내용 : ");
-					String body = sc.nextLine();
+					String body = sc.nextLine().trim();
 					
 					sql = new SecSql();
 					sql.append("UPDATE article");
